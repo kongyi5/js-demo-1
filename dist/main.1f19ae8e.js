4e84631d47aee72d0bb7910514b074b1e012a873
kongyi <jerrykongyi1997@gmail.com>
2020-06-11 11:05:43 +0800

    优化

--- dist/main.1f19ae8e.js
+++ dist/main.1f19ae8e.js
@@ -121,10 +121,31 @@ parcelRequire = (function (modules, cache, entry, globalName) {
 var div = document.createElement("div");
 div.className = "demo";
 document.body.appendChild(div);
+var dragging = false;
+var lastX;
+var lastY;
 
-document.body.onclick = function (e) {
-  div.style.top = e.clientY + "px";
-  div.style.left = e.clientX + "px";
+div.onmousedown = function (e) {
+  lastX = e.clientX;
+  lastY = e.clientY;
+  dragging = true;
+};
+
+document.onmousemove = function (e) {
+  if (dragging === true) {
+    var deltaX = e.clientX - lastX;
+    var deltaY = e.clientY - lastY;
+    var top = parseInt(div.style.top) || 0;
+    var left = parseInt(div.style.left) || 0;
+    div.style.top = top + deltaY + "px";
+    div.style.left = left + deltaX + "px";
+    lastX = e.clientX;
+    lastY = e.clientY;
+  }
+};
+
+div.onmouseup = function () {
+  dragging = false;
 };
 },{}],"../../../AppData/Local/Yarn/Data/global/node_modules/parcel/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
